48abf509a7b6d126cd0797c9225a2b10c15b886d
spageektti <155078792+spageektti@users.noreply.github.com>
2024-08-01 21:14:03 +0200

    Add basic website

---
+++ script.js
@@ -0,0 +1,5 @@
+function submit() {
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+    const email = document.querySelector('#email').value;
+}
